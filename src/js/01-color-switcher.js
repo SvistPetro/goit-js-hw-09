e78496f30d5165f35089a1760a9627d335519618
Petro Svist <svist.petro@gmail.com>
2023-06-28 22:05:57 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+function changeColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartClick(evt) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(changeColor, 1000);
+};
+
+function onStopClick(evt) {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+};
 
