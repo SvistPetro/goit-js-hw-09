e01f2753f00182296cbb682dc4d8f0feb5e6042b
Petro Svist <svist.petro@gmail.com>
2023-07-08 23:08:07 +0300

    Task 3 Finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+let formData = {};
+
+formEl.addEventListener('input', onFormInput);
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  
+  let delayEl = Number(formData.delay);
+  const stepEl = Number(formData.step);
+  const amountEl = Number(formData.amount);
+  
+  for (let i = 1; i <= amountEl; i +=1) {
+    createPromise(i, delayEl)
+    .then(({ position, delay }) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+
+  delayEl += stepEl;
+  };
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
